4f9b28316a74b5cf0b294df64d4c988783caea4c
David corticchiato <david.corticchiato@gmail.com>
2016-05-10 11:11:00 +0200

    show dev tools

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
- 
+var nwjsify = require('nwjs-browserify');
+
+
 gulp.task('dist', function() {
   gulp.src('lib/boot/app.js')
     .pipe(browserify({
-      insertGlobals : true,
-      debug : !gulp.env.production
     }))
     .pipe(gulp.dest('./gui/static'))
 });
