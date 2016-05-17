7203c04eebebd205bdcb7493539ff1c9632fa966
David corticchiato <david.corticchiato@gmail.com>
2016-05-17 13:37:46 +0200

    raw buttons and title for landing page

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,19 @@ var path = require('path');
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var babelify = require('babelify');
+var less = require('gulp-less');
 var rename = require('gulp-rename');
 
 
 gulp.task('default', function() {
   var staticDir = path.join(__dirname, 'gui', 'static');
+
+  gulp.src('./lib/style/main.less')
+    .pipe(less({
+      paths: [ './lib/style' ]
+    }))
+    .pipe(rename('squidmotion-app.css'))
+    .pipe(gulp.dest(staticDir));
 
   gulp.src('lib/boot/app.js')
     .pipe(browserify({
@@ -15,3 +23,4 @@ gulp.task('default', function() {
     .pipe(rename('squidmotion-app.js'))
     .pipe(gulp.dest(staticDir));
 });
+
