3972012e287ec6f2abaf70f938d0e90306fbca1a
David corticchiato <david.corticchiato@gmail.com>
2016-05-19 13:22:13 +0200

    serve all statics with gulp dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,34 @@
+var fs = require('fs');
 var path = require('path');
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var babelify = require('babelify');
+var express = require('express');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
 
-var staticDir = path.join(__dirname, 'gui', 'static');
+
+
 var specs = [
  {
+   /*
+    * LESS Processor
+    */
+
    src: './lib/style/main.less',
    processors: [
     less({
-      paths: [ './lib/style' ]
+      paths: ['./lib/style']
     }),
     rename('squidmotion-app.css')
    ]
  },
  {
-   src: 'lib/boot/app.js',
+   /*
+    * Browserify (with babel/react) processor
+    */
+
+   src: './lib/boot/app.js',
    processors: [
     browserify({
       transform: [babelify.configure({presets: ['es2015', 'react']})],
@@ -27,7 +38,6 @@ var specs = [
  }
 ];
 
-
 gulp.task('default', function() {
   specs.forEach(function (spec) {
     var stream = gulp.src(spec.src);
@@ -36,6 +46,25 @@ gulp.task('default', function() {
       stream = stream.pipe(processor);
     });
 
-    stream.pipe(gulp.dest(staticDir));
+    stream.pipe(gulp.dest('./gui/static'));
   });
+});
+
+gulp.task('dev', function() {
+  var app = express();
+
+  app.get(/^(.*)$/, function (req, res) {
+    var part = req.params[0];
+    var stream = null;
+
+    if (part === '' || part === '/') {
+      stream = fs.createReadStream('./gui/index.html');
+    } else {
+      stream = fs.createReadStream(path.join('./gui', part));
+    }
+
+    stream.pipe(res);
+  });
+
+  app.listen(8666);
 });
