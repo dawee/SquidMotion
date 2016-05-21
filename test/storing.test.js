c5e783cdd6536073f636f03278fd509dd3fc54f2
David Corticchiato <david.corticchiato@gmail.com>
2016-05-21 15:46:12 +0200

    stores send event after dispatching action

--- test/storing.test.js
+++ test/storing.test.js
@@ -1,12 +1,18 @@
 import assert from 'assert';
 import projectStore from '../lib/store/project';
 import * as action from '../lib/action';
+import * as constant from '../lib/constant';
 
 describe('storing', function () {
 
   it('should store when action is triggered', function () {
     action.createProject('foobar.svg');
     assert.equal(1, projectStore.projects.length);
+  });
+
+  it('should send an event when a project is created', function (done) {
+    projectStore.on(constant.PROJECT_CREATED, done);
+    projectStore.onCreate({svgFileName: 'foobar.svg'});
   });
 
 });
