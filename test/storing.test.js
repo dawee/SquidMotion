e2eae504439a8b192dd7f827bf8a8f85f14c1d15
David corticchiato <david.corticchiato@gmail.com>
2016-05-23 10:47:14 +0200

    move file reader to 'native' lib for tests

--- test/storing.test.js
+++ test/storing.test.js
@@ -1,18 +1,15 @@
 import assert from 'assert';
 import projectStore from '../lib/store/project';
 import * as action from '../lib/action';
-import * as constant from '../lib/constant';
 
 describe('storing', function () {
 
   it('should store when svgImport is triggered', function () {
-    action.importSVG('foobar.svg');
-    assert.equal(1, projectStore.projects.length);
-  });
+    projectStore.on('change', function () {
+      assert.equal(1, projectStore.projects.length);
+    });
 
-  it('should send an event when a project is created', function (done) {
-    projectStore.on('change', done);
-    projectStore.onCreate({svgFileName: 'foobar.svg'});
+    action.importSVG('foobar.svg');
   });
 
 });
